eb14ea439515f823a5973e117f097919c031aeaa
TiciaChance <laticia.chance@flatironschool.com>
2016-10-02 11:39:56 -0400

    get authorization for speech recognization from user

--- SevenThings/ViewController.swift
+++ SevenThings/ViewController.swift
@@ -16,10 +16,39 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var recordingButton: UIButton!
     
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        recordingButton.isEnabled = false
+        speechRecognizer?.delegate = self
+        
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            
+            var isButtonEnabled = false
+            
+            switch authStatus {  //5
+            case .authorized:
+                isButtonEnabled = true
+                
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+                
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+                
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.recordingButton.isEnabled = isButtonEnabled
+            }
+        }
     }
     
 
