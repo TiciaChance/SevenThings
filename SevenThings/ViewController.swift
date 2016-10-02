79114d417fa9046520cf90bc6a36222a79439121
TiciaChance <laticia.chance@flatironschool.com>
2016-10-02 11:30:53 -0400

    connect outlets

--- SevenThings/ViewController.swift
+++ SevenThings/ViewController.swift
@@ -6,20 +6,23 @@
 //  Copyright © 2016 Laticia Chance. All rights reserved.
 //
 
+
 import UIKit
+import Speech
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
+    
+    @IBOutlet weak var userPromptLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var recordingButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    @IBOutlet weak var recordingButtonTapped: UIButton!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
-
